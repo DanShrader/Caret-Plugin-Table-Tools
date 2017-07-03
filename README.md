7632c438ac255e672a26160c4a644f75b3c73d31
Dan Shrader <dan@shraderland.com>
2017-07-03 12:36:02 -0400

    Combined the two different plug-ins together to simplify the amount of extensions needed

--- README.md
+++ README.md
@@ -3,11 +3,68 @@ This Caret plugin currently allows for CSV to be converted to Markdown and soon 
 
 Once I get is wrapped up I'll put it on the chrome webstore.
 
+The options for [CSV To Markdown Table](https://github.com/donatj/CsvToMarkdownTable) are passed via "optionsCSV"
 
-## ## The following have contributed works
+
+## To use with Caret
+
+You'll need to add the following to your api.json file, under settings menu.  This plugin
+can pass the same options that JS-Beautify does in the 'options' tag.  **Note:** The id is the id found under extensions.
+
+### api.json
+
+```JavaScript
+{
+	"sampleMessage": {
+		"id": "extension id goes here",
+		"message": {
+			"data": "message can be any JSON object passable to chrome.runtime.sendMessageExternal"
+		}
+	},"CSV-to-MD": {
+		"id": "gaonahnbhbpdimakadahmbogdfdhlifg",
+		"message": {
+		  "command":"CSV-to-MD",
+			"optionsCSV": {
+				// "deliminator": "\t", //tab
+				"deliminator": ",", //,
+				// "header": false
+				"header": true
+			}
+		},
+		"sendEditorContext": true
+	}
+	,"Pretty-MD": {
+		"id": "gaonahnbhbpdimakadahmbogdfdhlifg",
+	  "message": {
+	    "command":"Pretty-MD"
+	  },
+		"sendEditorContext": true
+	}
+}
+```
+
+### menus.json
+
+This is optional, but allows the command to be accessable in menu system.
+
+```JavaScript
+{
+	"label": "MD Table Beautify",
+	"command": "api:execute",
+	"argument": "Pretty-MD"
+},
+{
+	"label": "Format SQL",
+	"command": "api:execute",
+	"argument": "format-SQL"
+}
+```
+
+
+
+## The following have contributed works
 
 Without the following, this plug-in would not exist.  Thanks to their works.
 
 - [Caret](https://github.com/thomaswilburn/Caret)
 - [CSV To Markdown Table](https://github.com/donatj/CsvToMarkdownTable) - their pattern is the base for the markdown formatting.
-
--- background.js
+++ background.js
@@ -10,55 +10,63 @@ var sendToCaret = function (command, argument, quiet) {
 chrome.runtime.onMessageExternal.addListener(
 	function (message, sender, sendResponse) {
 		var data = message.context.selection;
-		var options = message.options || {};
-		var deliminator = options.deliminator || ",";
-		var header = options.header || true;
-		// console.info(data);
-		var columns = data.split("\n");
-		var tabularData = [];
-		var maxRowLen = [];
-		columns.forEach(function (e, i) {
-			if (typeof tabularData[i] == "undefined") {
-				tabularData[i] = [];
-			}
-			var row = e.split("|");
-			row.forEach(function (ee, ii) {
-				if (typeof maxRowLen[ii] == "undefined") {
-					maxRowLen[ii] = 0;
-				}
-				if (ee.indexOf("--") !== -1) {
-					//tabularData[i][ii] = '-'.repeat(maxRowLen[ii]);
-					tabularData[i][ii] = "--";
-				} else {
-					maxRowLen[ii] = Math.max(maxRowLen[ii], ee.trim().length);
-					tabularData[i][ii] = ee.trim();
-				}
-			});
-		});
-		columns.forEach(function (e, i) {
-			if (typeof tabularData[i] == "undefined") {
-				tabularData[i] = [];
-			}
-			var row = e.split("|");
-			row.forEach(function (ee, ii) {
-				if (tabularData[i][ii].indexOf("--") !== -1) {
-					tabularData[i][ii] = '-'.repeat(maxRowLen[ii]);
-				} else {
-					// console.log("text String is : " + tabularData[i][ii]);
-					// console.log("data length is : " + tabularData[i][ii].length);
-					// console.log(" row length is : " + maxRowLen[ii]);
-					var whitespace = maxRowLen[ii] - tabularData[i][ii].length;
-					if (whitespace < 0) {
-						whitespace = 0
-					}
-					// console.log(whitespace);
-					tabularData[i][ii] = tabularData[i][ii] + ' '.repeat(whitespace);
-				}
-			});
-		});
+		var command = message.command || 'none';
 		var returnData = "";
-		tabularData.forEach(function (row, i) {
-			returnData = returnData + (row.join(" | ")).trim() + "\n";
-		});
+		// This is for formatting a Markdown Table
+		if (command === "Pretty-MD") {
+		// 	console.log('Pretty-MD')
+			// console.info(data);
+			var columns = data.split("\n");
+			var tabularData = [];
+			var maxRowLen = [];
+			columns.forEach(function (e, i) {
+				if (typeof tabularData[i] == "undefined") {
+					tabularData[i] = [];
+				}
+				var row = e.split("|");
+				row.forEach(function (ee, ii) {
+					if (typeof maxRowLen[ii] == "undefined") {
+						maxRowLen[ii] = 0;
+					}
+					if (ee.indexOf("--") !== -1) {
+						//tabularData[i][ii] = '-'.repeat(maxRowLen[ii]);
+						tabularData[i][ii] = "--";
+					} else {
+						maxRowLen[ii] = Math.max(maxRowLen[ii], ee.trim().length);
+						tabularData[i][ii] = ee.trim();
+					}
+				});
+			});
+			columns.forEach(function (e, i) {
+				if (typeof tabularData[i] == "undefined") {
+					tabularData[i] = [];
+				}
+				var row = e.split("|");
+				row.forEach(function (ee, ii) {
+					if (tabularData[i][ii].indexOf("--") !== -1) {
+						tabularData[i][ii] = '-'.repeat(maxRowLen[ii]);
+					} else {
+						var whitespace = maxRowLen[ii] - tabularData[i][ii].length;
+						if (whitespace < 0) {
+							whitespace = 0;
+						}
+						tabularData[i][ii] = tabularData[i][ii] + ' '.repeat(whitespace);
+					}
+				});
+			});
+			tabularData.forEach(function (row, i) {
+				returnData = returnData + (row.join(" | ")).trim() + "\n";
+			});
+		}
+		// This is for converting a CSV File to markdown
+		if (command === "CSV-to-MD") {
+		// 	console.log('CSV-to-MD')
+		// 	console.log(message)
+			var optionsCSV = message.optionsCSV || {};
+			var deliminator = optionsCSV.deliminator || ",";
+			var header = optionsCSV.header || true;
+			data = data.replace(/(?:\r\n|\r|\n)/g, '\n');
+			returnData = csvToMarkdown(data, deliminator, header);
+		}
 		sendToCaret('editor:insert', returnData, true);
 	});
